4a2ec9bb7e7e3767effcd26307e940768e6151cd
Mohamed Fathy <mohamed.fathii@hotmail.com>
2017-09-19 15:12:21 +0200

    add the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './home/posts/posts.component';
+import { CreatePostComponent } from './home/posts/create-post/create-post.component';
+import { EditPostComponent } from './home/posts/edit-post/edit-post.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent,
+    PostsComponent,
+    CreatePostComponent,
+    EditPostComponent,
+    ProfileComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
